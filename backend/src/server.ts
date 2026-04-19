e54ced5fc663b9c7e047acccc303eda0e5c698d5
lord-carlos <155855+lord-carlos@users.noreply.github.com>
2026-04-19 10:28:33 +0200

    docker changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ try {
   console.log('Starting server...');
 
   const app = express();
-  const PORT = process.env.PORT || 3001;
+  const PORT = 3001;
 
   app.use(cors());
   app.use(express.json());
